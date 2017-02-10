35630dbd0efb9a5a57445d37b8e7ec5b6315d540
climategames <code@climategames.net>
2017-02-10 16:30:44 +0100

    Deploy to new vps

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock '3.4.0'
 
 set :application, 'ClimateGames'
-set :repo_url, 'https://github.com/climategames/climategames.git'
+set :repo_url, 'git@github.com:climategames/climategames-website.git'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
